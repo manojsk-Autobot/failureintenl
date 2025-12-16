7ced99c9a46e3da127294236a8bd3e2e3098c539
root <root@IU9RFDP34>
2025-12-16 10:12:57 +0000

    removed all files

--- app/__init__.py
+++
@@ -1,4 +0,0 @@
-"""App package initialization."""
-from .main import app
-
-__all__ = ['app']
--- services/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Services package for handling business logic."""
-from .email_service import EmailService
-from .analysis_service import AnalysisService
-
-__all__ = ['EmailService', 'AnalysisService']
